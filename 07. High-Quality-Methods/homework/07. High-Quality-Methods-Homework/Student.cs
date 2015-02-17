70d009e3f236046784b0cc737806953461ed9605
micev <micevgeorgi@gmail.com>
2015-02-17 18:09:17 +0200

    add homework

---
+++ 07. High-Quality-Methods/homework/07. High-Quality-Methods-Homework/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Methods
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string birthdayDate { get; set; }
+
+        public bool IsOlderThan(Student other)
+        {
+            DateTime firstDate =
+                DateTime.Parse(this.birthdayDate);
+            DateTime secondDate =
+                DateTime.Parse(other.birthdayDate);
+            return firstDate < secondDate;
+        }
+    }
+}
